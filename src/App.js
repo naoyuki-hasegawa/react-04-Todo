5854ae7019c66b305e1b67d7e5c86fa65294a83d
naoyuki-hasegawa <naoyuki.hasegawa@freeeks.jp>
2024-04-14 09:29:29 +0900

    First Commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TodoList from "./components/TodoList";
+import { useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+import React from "react";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const todoNameRef = useRef();
+
+  const handleAddTodo = (e) => {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+     console.log(name);
+    setTodos((prevTodos) => {
+      //console.log(prevTodos);
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" name="" id="" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクの追加</button>
+      <button onClick={handleClear}>タスクの削除</button>
+      <p>タスクの残:{todos.filter((todo) => !todo.completed).length}</p>
+    </>
   );
-}
+};
 
 export default App;
